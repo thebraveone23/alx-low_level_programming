ae0c0bdd32ec785fc314350a9f83aa8edebd9c39
thebraveone23 <thebraveone.tn@gmail.com>
2023-07-14 06:09:15 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - A program that prints number 0 - 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int b = 0;
+
+	while (b <= 9)
+	{
+		printf("%d", b);
+		b++;
+	}
+	printf("\n");
+	return (0);
+}
