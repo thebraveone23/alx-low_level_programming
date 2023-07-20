d0ab66a9af826ca1c9fab55570f56d0270e0311e
thebraveone23 <thebraveone.tn@gmail.com>
2023-07-19 19:11:17 -0700

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
 /**
- * print_sign - Prints the sign of a number.
- * @n: The number of which the sign will be printed.
- *
- * Return: 1 if the number is greater than zero,
- *         0 if the number is zero,
- *         -1 if the number is less than zero.
+ * print_sign - entry point
+ * @c: character
+ * Description: return 1 if c is in lower case
+ * Return: 1 or 0
  */
-int print_sign(int n)
+int print_sign(int c)
 {
-	if (n > 0)
+	if (c > 0)
 	{
-		_putchar('45');
-		return (1);
+		_putchar('+');
+			return (1);
 	}
-	else if (n == 0)
+	else if (c == 0)
 	{
-		_putchar('45');
+		_putchar('0');
+		return (0);
+			}
+	else if (c < 0)
+	{
+		_putchar('-');
 		return (-1);
 	}
-	else
-	{
-		_putchar('45');
-		return (0);
-	}
+	return (0);
 }
