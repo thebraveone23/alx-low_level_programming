2909f463d25bc8c9deda9a6f924d6e5be0545d18
thebraveone23 <thebraveone.tn@gmail.com>
2023-07-21 19:13:58 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * Main - A program that print_line
+ * @n: The number of lines to draw
+ * Return: empty
+ */
+
+void print_line(int n)
+
+{
+
+	int x;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0; x < n; x++)
+	{
+	_putchar(95);
+	}
+	_putchar('\n');
+	}
+}
