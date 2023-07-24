d77c47a3464ca020f506204cd83dea7770189ccf
thebraveone23 <thebraveone.tn@gmail.com>
2023-07-24 11:31:53 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * Main - reset_pointer_to_98
+ * Submain - the pointer should point 98
+ * @n: pointer should be at variable update
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
