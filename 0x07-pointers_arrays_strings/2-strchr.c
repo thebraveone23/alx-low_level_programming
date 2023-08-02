d22d408d633c8a6f65ed85cbacc98e6372df9318
thebraveone23 <thebraveone.tn@gmail.com>
2023-08-01 19:24:16 -0700

    2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strchr - entry point
+ * @str: the given string
+ * @c: the given string
+ *
+ * Return: Always 0 (Success)
+ * ------- (Fail) return a null pointer
+ */
+char *_strchr(char *str, char c)
+{
+	int i - 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return(0);
+}	
