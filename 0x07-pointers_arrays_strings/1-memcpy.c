969920887ebfcf0c1e0d8b5960117fd93a7ed44a
thebraveone23 <thebraveone.tn@gmail.com>
2023-08-01 18:48:51 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * memcpy - a program that  copy memory area
+ *
+ * @dest: memory from source
+ * @src: copied memory
+ * @n: num of byte
+ *
+ * Return: copy memory with byte change
+ */
+char *memcpy(char *dest, char *src, unsigned int n)
+{
+	int l = 0;
+	int i = n;
+
+	for (l < i; l++)
+	{
+		dest[l] = src[l];
+		n--;
+	}
+	return (dest);
+}
