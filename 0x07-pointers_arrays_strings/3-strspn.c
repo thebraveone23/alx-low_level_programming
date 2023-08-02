375669a0f62ed50b0ebf8a7868dc024cef85ef9c
thebraveone23 <thebraveone.tn@gmail.com>
2023-08-01 19:39:12 -0700

    0x07. C - Even more pointers, arrays and strings 9 tasks

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,8 @@
+/**
+ * _strchr - locates a character in a given string
+ * @str: the given string
+ * @c: the given string
+ *
+ * Return: (Success) a pointer to the first occurence of c
+ * ------- (Fail) return a null pointer
+ */
